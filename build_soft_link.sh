1ba069225d9213f46a20d16bbca00f7d876376aa
WuZhigang <hnhbwzg@gmail.com>
2024-03-19 10:17:12 +0800

    1. 脚本可移植; 2.config_list支持注释

--- build_soft_link.sh
+++ build_soft_link.sh
@@ -1,3 +1,23 @@
-for FILE in `cat config_list`;do
-    ln -snf ~/dotfiles/$FILE ~/$FILE
-done
+#!/bin/bash
+mkdir ~/dotfiles
+mkdir -p ~/bin/tmux-session
+
+# 获取脚本所在的绝对路径
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+# 遍历配置文件中的每一行
+while IFS= read -r FILE; do
+    # 跳过以'#'开头的行
+    if [[ $FILE =~ ^# ]]; then
+        continue
+    fi
+    # 创建或更新符号链接
+    ln -snf ~/dotfiles/"$FILE" ~/"$FILE"
+done < "$SCRIPT_DIR/config_list"
+
+#for FILE in `cat config_list`;do
+#    if [[ $FILE =~ '#' ]];then
+#	    continue;
+#    fi
+#    ln -snf ~/dotfiles/$FILE ~/$FILE
+#done
