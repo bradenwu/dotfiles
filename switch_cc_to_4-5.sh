3f4ab22d622a9da7158bb5db143a2a33a67e0d97
hnhbwzg <hnhbwzg@gmail.com>
2025-09-23 23:19:20 +0800

    add claude code related function

---
+++ switch_cc_to_4-5.sh
@@ -0,0 +1,10 @@
+unset http_proxy
+unset HTTP_PROXY
+unset HTTPS_PROXY
+unset https_proxy
+
+unset ANTHROPIC_MODEL
+unset ANTHROPIC_SMALL_FAST_MODEL
+
+export ANTHROPIC_BASE_URL=http://open.bigmodel.cn/api/anthropic
+export ANTHROPIC_AUTH_TOKEN=890582721cb6421ea6ccf3b656831341.hRczO0AXbnT0JQ9D
---
+++ switch_cc_to_k2.sh
@@ -0,0 +1,20 @@
+# Linux/macOS 启动高速版 kimi-k2-turbo-preview 模型
+unset http_proxy
+unset HTTP_PROXY
+unset HTTPS_PROXY
+unset https_proxy
+export ANTHROPIC_BASE_URL=https://api.moonshot.cn/anthropic
+export ANTHROPIC_AUTH_TOKEN=sk-tIbaAr5NRBh7aMdswGDLkpG4pmFrZfn9DujgEmMtRI4bNTzT
+export ANTHROPIC_MODEL=kimi-k2-turbo-preview
+export ANTHROPIC_SMALL_FAST_MODEL=kimi-k2-turbo-preview
+
+#  ~/.claude/settings.json
+#{
+#  "includeCoAuthoredBy": false,
+#  "env": {
+#    "ANTHROPIC_AUTH_TOKEN": "890582721cb6421ea6ccf3b656831341.hRczO0AXbnT0JQ9D",
+#    "ANTHROPIC_BASE_URL": "https://open.bigmodel.cn/api/anthropic",
+#    "API_TIMEOUT_MS": "3000000"
+#  }
+#}
+
